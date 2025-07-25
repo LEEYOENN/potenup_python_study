de0b5c0ffe1d1b63b6a6050e22ddbce3d0d3478f
LEEYOENN <rofttm0033@gmail.com>
2025-07-25 18:26:45 +0900

    📖[Study] 7/25 python study- selenium, beatifulsoup

---
+++ inregular_data_practice/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from inregular-data-practice!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ work/main.py
@@ -0,0 +1,10 @@
+def main():
+    print("Hello from work!")
+    import pandas as pd
+    data = pd.DataFrame({'인사':['안녕하세요', 'hello']})
+    print(data)
+    
+
+
+if __name__ == "__main__":
+    main()
